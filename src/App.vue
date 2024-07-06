f19974614cd695ec50093cb58f01299f21279f6b
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-06 23:00:13 +0200

    try features in app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to HelloBooks" />
+    <SingleBook book="featuredBook" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SingleBook from './components/SingleBook.vue'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        HelloWorld,
+        SingleBook
+    },
+    data() {
+        return {
+            featuredBook: {
+                title: "The Great Gatsby",
+                author: "F. Scott Fitzgerald",
+                coverUrl: "/images/great-gatsby-cover.jpg",
+                rating: "4.5",
+                genre: "Classic Literature"
+            }
+        }
     }
 }
 </script>
