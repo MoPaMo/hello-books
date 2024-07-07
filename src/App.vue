ff17605bc360199c985f7d51368c1a4587449f2b
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-07 11:46:16 +0200

    clean up app

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,8 @@
 </template>
 
 <script>
-import SingleBook from './components/SingleBook.vue'
-import BookList from './components/list.vue'
-import XLCategories from './components/XLCategories.vue'
+
 import Bar from './components/bar.vue'
-import BookPage from './components/bookpage.vue'
 export default {
     name: 'App',
     components: {
@@ -24,40 +21,8 @@ export default {
 
 
     },
-    data() {
-        return {
-            featuredBook: {
-                title: "The Great Gatsby",
-                author: "F. Scott Fitzgerald",
-                coverUrl: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF894,1000_QL80_.jpg",
-                rating: "4.5",
-                genre: "Classic Literature"
-            },
-            books: [
-                {
-                    title: "The Great Gatsby",
-                    author: "F. Scott Fitzgerald",
-                    coverUrl: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF894,1000_QL80_.jpg",
-                    rating: "4.5",
-                    genre: "Classic Literature"
-                },
-                {
-                    title: "To Kill a Mockingbird",
-                    author: "Harper Lee",
-                    coverUrl: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF894,1000_QL80_.jpg",
-                    rating: "4.5",
-                    genre: "Classic Literature"
-                },
-                {
-                    title: "The Catcher in the Rye",
-                    author: "J. D. Salinger",
-                    coverUrl: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF894,1000_QL80_.jpg",
-                    rating: "4.5",
-                    genre: "Classic Literature"
-                }
-            ]
-        }
-    }
+
+
 }
 </script>
 
