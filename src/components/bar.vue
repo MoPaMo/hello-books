864257ae72d3ba188258102d56b66752973d7068
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-07 11:51:02 +0200

    use real bar

--- src/components/bar.vue
+++ src/components/bar.vue
@@ -1,32 +1,35 @@
 <template>
     <div
         class="fixed bottom-0 left-0 right-0 bg-white dark:bg-zinc-800 border-t dark:border-zinc-700 flex justify-around py-2">
-        <button class="flex flex-col items-center">
-            <img src="https://placehold.co/24x24" alt="Today" />
-            <span class="text-xs">Today</span>
-        </button>
-        <button class="flex flex-col items-center text-blue-500 dark:text-blue-400">
-            <img src="https://placehold.co/24x24?text=✨" alt="Fiction" />
-            <span class="text-xs">Fiction</span>
-        </button>
-        <button class="flex flex-col items-center">
-            <img src="https://placehold.co/24x24" alt="Non-Fictio" />
-            <span class="text-xs">Non-Fiction</span>
-        </button>
-        <button class="flex flex-col items-center">
-            <img src="https://placehold.co/24x24" alt="Classics" />
-            <span class="text-xs">Classics</span>
-        </button>
-        <button class="flex flex-col items-center">
-            <img src="https://placehold.co/24x24" alt="Search" />
-            <span class="text-xs">Search</span>
+        <button v-for="(item, index) in items" :key="index" @click="setHighlight(index)"
+            class="flex flex-col items-center"
+            :class="{ 'text-blue-500 dark:text-blue-400': highlightedIndex === index }">
+            <img :src="item.imgSrc" :alt="item.text" />
+            <span class="text-xs">{{ item.text }}</span>
         </button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Bar'
+    name: 'Bar',
+    data() {
+        return {
+            highlightedIndex: 1,
+            items: [
+                { text: 'Today', imgSrc: 'https://placehold.co/24x24' },
+                { text: 'Fiction', imgSrc: 'https://placehold.co/24x24?text=✨' },
+                { text: 'Non-Fiction', imgSrc: 'https://placehold.co/24x24' },
+                { text: 'Classics', imgSrc: 'https://placehold.co/24x24' },
+                { text: 'Search', imgSrc: 'https://placehold.co/24x24' },
+            ],
+        }
+    },
+    methods: {
+        setHighlight(index) {
+            this.highlightedIndex = index
+        },
+    },
 }
 </script>
 
