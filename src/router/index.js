3fbfe4b7dc9b90b9f848c940f00f7ae3be0398da
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-07 13:10:02 +0200

    add 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import XLCategories from "./../components/XLCategories.vue";
+import NotFound from "./../views/NotFound.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: XLCategories,
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
   },
 ];
 
